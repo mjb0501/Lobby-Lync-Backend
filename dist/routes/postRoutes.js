3efb44484e705b89ed9cdffb35874bb772941c5c
mjb0501 <mjb0501@gmail.com>
2024-12-23 12:41:03 -0600

    Removed dist folder

--- dist/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const postController_1 = require("../controllers/postController");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const router = (0, express_1.Router)();
-router.post('/', authMiddleware_1.authenticate, postController_1.createPost);
-exports.default = router;
